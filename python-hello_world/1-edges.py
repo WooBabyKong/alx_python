89b4273fcf2a26f59ccd9c1f666a4736d9536321
WooBabyKong <veenaose@gmail.com>
2023-07-15 18:49:48 +0100

    1-edge task done

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,14 +1,7 @@
 word = "Holberton"
-
-# Extract the first 3 letters of the word
 word_first_3 = word[:3]
-
-# Extract the last 2 letters of the word
 word_last_2 = word[-2:]
-
-# Extract the middle word without the first and last letters
 middle_word = word[1:-1]
-
 # Print the results
 print("First 3 letters:", word_first_3)
 print("Last 2 letters:", word_last_2)

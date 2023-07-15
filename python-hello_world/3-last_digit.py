03ec8cba1346201bdd4ba13d1092902281a7fd9a
WooBabyKong <veenaose@gmail.com>
2023-07-15 22:47:07 +0100

    task 3

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,17 @@
+import random
+
+# Generate a random signed number
+number = random.randint(-10000, 10000)
+
+# Extract the last digit of the number
+last_digit = abs(number) % 10
+
+# Print the last digit of the number along with the appropriate message
+print("The string Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")

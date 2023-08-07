ce293a287a0a2b4b95a1844f51bc3d53eb9631fb
WooBabyKong <veenaose@gmail.com>
2023-08-07 17:02:55 +0100

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,14 @@
 import sys
+from add_0 import add
 
 def main():
     argv = sys.argv[1:]
     num_args = len(argv)
 
-    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}{'.' if num_args == 0 else ':'}")
+    print("Number of argument{}: {}:".format('s' if num_args != 1 else '', num_args))
 
     for i, arg in enumerate(argv, start=1):
-        print(f"{i}: {arg}")
+        print("{}: {}".format(i, arg))
 
 if __name__ == "__main__":
     main()

9c5354856439c6452381ad04460fb329f8a292e3
WooBabyKong <veenaose@gmail.com>
2023-08-07 21:43:17 +0100

    update

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,10 +1,2 @@
-def print_sorted_dictionary(my_dict):
-    if my_dict is not None:
-        keys = sorted(my_dict.keys())
-        for key in keys:
-            print("{}: {}".format(key, my_dict[key]))
-
-new_dict = {'a': 'A', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e'}
-print_sorted_dictionary(new_dict)
-print("xx")
-print_sorted_dictionary(new_dict)
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value

7efc9207dbc5640a91c7b8237057fc7f8d20af84
WooBabyKong <veenaose@gmail.com>
2023-08-07 20:54:11 +0100

    update

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -2,5 +2,6 @@ def raise_exception_msg(message=""):
     try:
         raise NameError(message)
     except NameError:
-        print(message)
-raise_exception_msg("This is a name exception.")
+        pass
+
+    print(message)

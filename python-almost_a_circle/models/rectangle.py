8ecab2b6d232bed5d4f707a3b71785a6d1903d5e
WooBabyKong <veenaose@gmail.com>
2023-08-09 13:18:12 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,97 +1,24 @@
 #!/usr/bin/python3
 """Module documentation"""
-from models.base import Base
+from models.rectangle import Rectangle
 
-class Rectangle(Base):
-    """Rectangle class inherits from Base"""
-    def __init__(self, width, height, x=0, y=0, id=None):
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        super().__init__(size, size, x, y, id)
 
     @property
-    def width(self):
-        """Getter for width"""
-        return self.__width
+    def size(self):
+        """Getter for size (overrides Rectangle's width)"""
+        return self.width
 
-    @width.setter
-    def width(self, value):
-        """Setter for width"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Getter for height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter for height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter for x"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter for x"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter for y"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter for y"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Calculate and return the area of the rectangle"""
-        return self.__width * self.__height
-
-    def display(self):
-        """Display the rectangle using '#' characters"""
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+    @size.setter
+    def size(self, value):
+        """Setter for size (overrides Rectangle's width)"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        """String representation of the rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
-                                                       self.__width, self.__height)
-
-    def update(self, *args, **kwargs):
-        """Update attributes based on arguments"""
-        if args:
-            attr_names = ['id', 'width', 'height', 'x', 'y']
-            for i, value in enumerate(args):
-                if i < len(attr_names):
-                    setattr(self, attr_names[i], value)
-        else:
-            for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+        """String representation of the square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)

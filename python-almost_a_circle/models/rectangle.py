066ebc0cffba47d0b23292e04911c6a5f64d8a82
WooBabyKong <veenaose@gmail.com>
2023-08-09 13:09:00 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,8 @@ class Rectangle(Base):
         """Display the rectangle using '#' characters"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """String representation of the rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
+                                                       self.__width, self.__height)

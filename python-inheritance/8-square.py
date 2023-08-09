cb92ad49baffcef89f8a6a85c31801f6c0679f0e
WooBabyKong <veenaose@gmail.com>
2023-08-09 14:13:44 +0100

    update

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,56 +1,28 @@
 #!/usr/bin/python3
-"""
-Module that defines the BaseGeometry and Rectangle classes.
-"""
+"""Defines a class Square."""
 
-class BaseGeometry:
-    """
-    A class named BaseGeometry.
-    """
+Rectangle = __import__('9-rectangle').Rectangle
 
-    def area(self):
-        """
-        Method that raises an exception.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Method that validates value.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-class Rectangle(BaseGeometry):
-    """
-    A class named Rectangle that inherits from BaseGeometry.
-    """
-
-    def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance.
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Returns the area of the rectangle.
-        """
-        return self.__width * self.__height
 
 class Square(Rectangle):
-    """
-    A class named Square that inherits from Rectangle.
-    """
-
+    """Represents a square."""
+    
     def __init__(self, size):
-        """
-        Initializes a Square instance.
+        """Initialize a new square instance.
+        
+        Args:
+            size (int): The size of the square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+
+if __name__ == "__main__":
+    my_square = Square(5)
+    print(my_square)
+    print(my_square.area())

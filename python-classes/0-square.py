80151eec4d132184489020746e5b39dc44bd9ceb
WooBabyKong <veenaose@gmail.com>
2023-08-09 13:30:22 +0100

    Update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+"""Module documentation"""
+
 class Square:
+    """Square class with private size attribute"""
     def __init__(self, size):
+        """Class constructor"""
         self.__size = size
-        

4254710bc0f39f40064fe77572387442c1af229a
WooBabyKong <veenaose@gmail.com>
2023-08-10 18:50:01 +0100

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
 import requests
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
-    
+    content = response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type:", type(content))
+    print("\t- content:", content)
